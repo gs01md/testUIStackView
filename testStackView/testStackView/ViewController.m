64ca0c3f2f23e720195628b44e1c9bbb16ebfd94
gs01md <gs01md@163.com>
2015-12-25 11:08:12 +0800

    创建初始程序

---
+++ testStackView/testStackView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testStackView
+//
+//  Created by WoodGao on 15/12/25.
+//  Copyright © 2015年 wood. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
